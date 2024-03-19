4f85dff33f2186245615c633d4ca246a75ef0fdf
Aidaar <aidar21mamatov@gmail.com>
2024-03-19 17:33:50 +0600

    add react project, and first commit

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import	'./Cart.css'
+import img1 from '../../assets/Cart/img1.jpg'
+
+
+const Cart = () => {
+	return (
+		<div>
+		<h1>This is cart</h1>
+		]<img src={img1} alt="" />
+		</div>
+	)
+}
+
+export default Cart
