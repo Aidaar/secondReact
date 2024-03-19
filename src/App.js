9859966415ef229133a82deeae0ab98790d3e157
Aidaar <aidar21mamatov@gmail.com>
2024-03-19 18:42:58 +0600

    add text on lorem

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
   return (
     <div className="App">
 			<Form />
-			<Cart />
+			<Cart />	
+			<div>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium et voluptate, voluptas consectetur alias vel rem libero veniam dolor sint doloribus doloremque corporis voluptatem maxime, ea neque velit labore perspiciatis saepe aut tempore placeat ipsa cumque. Non ipsum quibusdam id nisi dolore eum voluptate vel, quo labore repudiandae at, excepturi sint magni perferendis maxime facilis minima voluptas! Quia laborum commodi ea. Similique quidem, corrupti, enim quos qui impedit perspiciatis dicta temporibus a obcaecati molestiae! Numquam debitis a aliquam aut quasi! Voluptates modi veniam dolorum eveniet iure incidunt harum nihil ad earum ex, excepturi labore hic velit fugiat quis inventore amet.
+			</div>
     </div>
   );
 }
